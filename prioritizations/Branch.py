486f4ae7ec782f8a5b9a7010f784a6a82a820a2f
lilott8 <lilott8@gmail.com>
2015-11-14 21:23:12 -0800

    branch coverage is complete!

--- prioritizations/Branch.py
+++ prioritizations/Branch.py
@@ -12,48 +12,45 @@ class Branch(Prioritization, object):
 
     def __init__(self, tests):
         Prioritization.__init__(self, tests)
+        self.results = {'statements': [], 'branches': []}
 
-        self.build_statement_coverage_set()
         self.build_branch_coverage_set()
+        self.build_statement_coverage_set()
         pass
 
-
     """
-    Is this ugly? yes
-    does this greatly simplify the detection of branch coverage? absolutely
-    There are a few flaws that need to be ironed out from this particular logic.
-    test cases can be added multiple times
+    percent is monotonic.  Thus, we only add to our results when our coverage is
+    greater than the percent.  If it's not we can safely ignore it!
     """
     def build_statement_coverage_set(self):
-        length = len(self.tests)
-        for x in range(0, len(self.tests)):
-            y = x+1
-            if y < length:
-                statements_covered_x = set(self.tests[x].get('meta').get('statements').get('covered'))
-                branches_covered_x = set(self.tests[x].get('meta').get('branches').get('covered'))
-                statements_covered_y = set(self.tests[y].get('meta').get('statements').get('covered'))
-                branches_covered_y = set(self.tests[y].get('meta').get('branches').get('covered'))
+        percent = 0.0
+        for index in self.tests:
+            index_total_coverage = float(len(index.get('meta').get('statements').get('covered')) + \
+                        len(index.get('meta').get('statements').get('not')))
+            index_coverage = float(len(index.get('meta').get('statements').get('covered')))
 
-                branches_covered_set = list(branches_covered_x - branches_covered_y)
-                statements_covered_set = list(statements_covered_x - statements_covered_y)
-                # Statements covered
-                if len(statements_covered_set) > 0:
-                    for i in statements_covered_set:
-                        if i in statements_covered_x:
-                            self.results.append(self.tests[x])
-                        elif i in statements_covered_y:
-                            self.results.append(self.tests[y])
-                # Branches covered
-                if len(branches_covered_set) > 0:
-                    for i in branches_covered_set:
-                        if i in branches_covered_x:
-                            self.results.append(self.tests[x])
-                        elif i in statements_covered_y:
-                            self.results.append(self.tests[y])
-                    pass
-        pass
+            coverage = float(index_coverage / index_total_coverage)
 
+            # force this to be monotonic!
+            if coverage > percent:
+                percent = coverage
+                self.results.get('statements').append({'id': index.get('meta').get('id'),
+                                                       'statements': index.get('statements')})
+    """
+    This is the same as build_statement_coverage_set,
+    but separate for ease of use and flexibility
+    """
     def build_branch_coverage_set(self):
-        for x in range(0, len(self.tests)):
-            print x
-        pass
+        percent = 0.0
+        for index in self.tests:
+            index_total_coverage = float(len(index.get('meta').get('branches').get('covered')) + \
+                        len(index.get('meta').get('branches').get('not')))
+            index_coverage = float(len(index.get('meta').get('branches').get('covered')))
+
+            coverage = float(index_coverage / index_total_coverage)
+
+            # force this to be monotonic!
+            if coverage > percent:
+                percent = coverage
+                self.results.get('branches').append({'id': index.get('meta').get('id'),
+                                                    'branches': index.get('branches')})
